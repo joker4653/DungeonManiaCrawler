30698c4a3dab5773d64d56671cfd511650587cbb
Holly <z5359218@ad.unsw.edu.au>
2022-07-04 23:04:20 +1000

    added strategy pattern interface for move

---
+++ src/main/java/dungeonmania/MovingStrategy.java
@@ -0,0 +1,7 @@
+package dungeonmania;
+
+import java.util.List;
+
+public interface MovingStrategy {
+    public void move(List<Entity> listOfEntities);
+}
--- src/main/java/dungeonmania/Spider.java
+++ src/main/java/dungeonmania/Spider.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Spider extends MovingEntity {
+public class Spider extends MovingEntity implements MovingStrategy {
     private boolean isClockwise = true;
     private Position nextPosition;
     private int currHealth;
