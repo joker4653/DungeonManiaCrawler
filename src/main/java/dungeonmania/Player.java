805c4ebf65f7d7c19c70bab272f0095a17cb6750
Laura Wrigley-Carr <laura@profound.net>
2022-07-10 17:36:23 +1000

    dev: Implemented inventory for player.

--- src/main/java/dungeonmania/Player.java
+++ src/main/java/dungeonmania/Player.java
@@ -2,18 +2,35 @@ package dungeonmania;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import dungeonmania.util.Position;
 import dungeonmania.util.Direction;
 
 public class Player extends MovingEntity {
 
+    ArrayList<Entity> inventory = new ArrayList<Entity>();
+    HashMap<String, Integer> ActiveStates = new HashMap<String, Integer>();
+
     public Player(int x, int y) {
         super.setEntityID(UUID.randomUUID().toString());
         super.setInteractable(false);
         super.setEntityType("player");
         super.setCurrentLocation(new Position(x, y));
+    }
+
+    public ArrayList<Entity> getInventory() {
+        return inventory;
+    } 
+
+    public void addItem(Entity item) {
+        inventory.add(item);
+    }
+
+    public void removeItem(Entity item) {
+        inventory.remove(item);
     }
 
 
@@ -30,11 +47,21 @@ public class Player extends MovingEntity {
     private boolean legalMove(List<Entity> listOfEntities, Position next) {
         // TODO Reimplement the checking w/ duck typing from jsons, or alternate. Currently dodge.
 
-        for (Entity currEntity: listOfEntities) {
-            Position currPos = currEntity.getCurrentLocation();
-            if (currPos.equals(next) && !canStepOn(currEntity.getEntityType())) {
+
+        List<Entity> entitiesHere = listOfEntities.stream().filter(e -> e.getCurrentLocation().equals(next)).collect(Collectors.toList());
+
+        ArrayList<Entity> items = new ArrayList<Entity>();
+        for (Entity currEntity : entitiesHere) {
+            if (!canStepOn(currEntity.getEntityType())) {
                 return false;
-            } 
+            } else if (currEntity instanceof CollectableEntity) {
+                items.add(currEntity);
+            }
+        }
+
+        for (Entity curr : items) {
+            this.addItem(curr);
+            listOfEntities.remove(curr);
         }
 
         return true;
@@ -51,6 +78,7 @@ public class Player extends MovingEntity {
         legal.add("door_open");
         legal.add("spider");
         legal.add("mercenary");
+        legal.add("treasure");
 
         for (String legalType : legal) {
             if (type == legalType) {
