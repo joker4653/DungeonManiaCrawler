027ab5ed895604db3be3a799caf3422dc6c17028
Ahmed Mian <ahmedmian217@gmail.com>
2022-07-11 18:27:20 +1000

    Implemented door test

---
+++ src/test/java/dungeonmania/DoorTests.java
@@ -0,0 +1,46 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class DoorTests {
+    @Test
+    @DisplayName("Tests that player is able to move boulder successfully")
+    public void testBoulderMovementBlockage() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_DoorsKeysTest_useKeyWalkThroughOpenDoor", "c_DoorsKeysTest_useKeyWalkThroughOpenDoor");
+        
+        Position expectedPlayerPos = new Position(3, 1);
+
+        // Player picks up key
+        dmc.tick(Direction.RIGHT);
+        // Player goes through door
+        dmc.tick(Direction.RIGHT);
+
+        Position actualPlayerPos = getEntities(res, "player").get(0).getPosition();
+        assertEquals(expectedPlayerPos, actualPlayerPos);
+    }
+}
