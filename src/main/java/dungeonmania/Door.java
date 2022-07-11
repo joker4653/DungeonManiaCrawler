7d13d8556ac855271875462eac6fb31dcc55f304
Ahmed Mian <ahmedmian217@gmail.com>
2022-07-11 18:28:16 +1000

    Implemented door

--- src/main/java/dungeonmania/Door.java
+++ src/main/java/dungeonmania/Door.java
@@ -2,12 +2,15 @@ package dungeonmania;
 
 import java.util.UUID;
 
+import com.google.gson.JsonObject;
+
 import dungeonmania.util.Position;
 
 public class Door extends StaticEntity {
     private boolean isLocked;
+    private int keyID;
 
-    public Door(int x, int y) {
+    public Door(int x, int y, int keyID) {
         super.setCanZombieBeOnThisEntity(false);
         super.setCanSpiderBeOnThisEntity(true);
         super.setEntityID(UUID.randomUUID().toString());
@@ -15,6 +18,9 @@ public class Door extends StaticEntity {
         super.setEntityType("door");
         super.setCurrentLocation(new Position(x, y));
         super.setCanMercBeOnThisEntity(false);
+        this.keyID = keyID;
+        this.setLocked(true);
+        this.setCanBlockMovement(false);
     }
 
     public boolean isLocked() {
@@ -33,4 +39,11 @@ public class Door extends StaticEntity {
         this.isLocked = isLocked;
     }
 
+    public int getKeyID() {
+        return keyID;
+    }
+
+    public void setKeyID(int keyID) {
+        this.keyID = keyID;
+    }
 }
