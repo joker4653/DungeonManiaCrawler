34565beac73bb6442037cfb996797615bc5b0566
Ahmed Mian <ahmedmian217@gmail.com>
2022-07-16 13:40:02 +1000

    Implemented exit class

--- src/main/java/dungeonmania/Exit.java
+++ src/main/java/dungeonmania/Exit.java
@@ -1,5 +1,29 @@
 package dungeonmania;
 
-public class Exit {
-    
+import java.util.List;
+import java.util.UUID;
+import dungeonmania.util.Position;
+
+public class Exit extends StaticEntity {
+    private boolean exitState;
+
+    public Exit(int x, int y) {
+        super.setCanSpiderBeOnThisEntity(false);
+        super.setCanZombieBeOnThisEntity(false);
+        super.setCurrentLocation(new Position(x, y));
+        super.setEntityID(UUID.randomUUID().toString());
+        super.setEntityType("exit");
+        super.setInteractable(false);
+        super.setCanMercBeOnThisEntity(false);
+        this.setCanBlockPlayerMovement(false); 
+        this.setExitState(false);
+    }
+
+    public boolean isExitState() {
+        return exitState;
+    }
+
+    public void setExitState(boolean exitState) {
+        this.exitState = exitState;
+    }
 }
