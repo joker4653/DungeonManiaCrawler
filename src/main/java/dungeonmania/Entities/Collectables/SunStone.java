728de1b280312388dedecbd4cd8cebc5d9decac6
Joseph Fabrello <z5362697@ad.unsw.edu.au>
2022-07-24 17:57:32 +1000

    basic class made

---
+++ src/main/java/dungeonmania/Entities/Collectables/SunStone.java
@@ -0,0 +1,16 @@
+package dungeonmania.Entities.Collectables;
+
+import java.util.UUID;
+
+import dungeonmania.util.Position;
+
+public class SunStone extends CollectableEntity {
+    
+    public SunStone(int x, int y) {
+        super();
+        super.setEntityID(UUID.randomUUID().toString());
+        super.setEntityType("sun_stone");
+        super.setCurrentLocation(new Position(x, y));
+        super.setCollectableEntity(true);
+    }
+}
