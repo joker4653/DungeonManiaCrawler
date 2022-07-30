e1cc187d274b6a40b7784e2408d4e8344c00b103
Ahmed Mian <ahmedmian217@gmail.com>
2022-07-30 17:29:51 +1000

    fixed boulder tests and added more

--- src/test/java/dungeonmania/BoulderTests.java
+++ src/test/java/dungeonmania/BoulderTests.java
@@ -53,23 +53,89 @@ public class BoulderTests {
 
     @Test
     @DisplayName("Basic boulder push up")
-    public void basicBoulderPush() {
+    public void basicBoulderPushUp() {
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse res = dmc.newGame("d_boulderTest_pushUp", "c_playerTest_basicMovement");
 
-        Position expectedBoulderPos = new Position(1, 3);
-        Position actualBoulderPos = getEntities(res, "boulder").get(0).getPosition();
-        Position expectedPlayerPos = new Position(1, 2);
+        Position expectedPlayerPos = new Position(1, 3);
         Position actualPlayerPos = getEntities(res, "player").get(0).getPosition();
-        assertEquals(expectedBoulderPos, actualBoulderPos);    
-        assertEquals(expectedPlayerPos, actualPlayerPos);
+        Position expectedBoulderPos = new Position(1, 2);
+        Position actualBoulderPos = getEntities(res, "boulder").get(0).getPosition();
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulderPos, actualBoulderPos); 
 
         res = dmc.tick(Direction.UP);
-        expectedBoulderPos = new Position(1, 4);
-        actualBoulderPos = getEntities(res, "boulder").get(0).getPosition();
-        expectedPlayerPos = new Position(1, 3);
-        assertEquals(expectedBoulderPos, actualBoulderPos);    
+        
+        expectedPlayerPos = new Position(1, 2);
+        actualPlayerPos = getEntities(res, "player").get(0).getPosition();  
+        expectedBoulderPos = new Position(1, 1);
+        actualBoulderPos = getEntities(res, "boulder").get(0).getPosition(); 
         assertEquals(expectedPlayerPos, actualPlayerPos);
+        assertEquals(expectedBoulderPos, actualBoulderPos);
+    }
 
+    @Test
+    @DisplayName("Basic boulder push down")
+    public void basicBoulderPushDown() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_boulderTest_pushDown", "c_playerTest_basicMovement");
+
+        Position expectedPlayerPos = new Position(1, 2);
+        Position actualPlayerPos = getEntities(res, "player").get(0).getPosition();
+        Position expectedBoulderPos = new Position(1, 3);
+        Position actualBoulderPos = getEntities(res, "boulder").get(0).getPosition();
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulderPos, actualBoulderPos); 
+
+        res = dmc.tick(Direction.DOWN);
+        
+        expectedPlayerPos = new Position(1, 3);
+        actualPlayerPos = getEntities(res, "player").get(0).getPosition();  
+        expectedBoulderPos = new Position(1, 4);
+        actualBoulderPos = getEntities(res, "boulder").get(0).getPosition(); 
+        assertEquals(expectedPlayerPos, actualPlayerPos);
+        assertEquals(expectedBoulderPos, actualBoulderPos);
+    }
+
+    @Test
+    @DisplayName("Check if player is unable to move two boulders")
+    public void doubleBoulderBlock() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_boulderTest_doubleBoulder", "c_playerTest_basicMovement");
+
+        Position expectedPlayerPos = new Position(1, 2);
+        Position actualPlayerPos = getEntities(res, "player").get(0).getPosition();
+        Position expectedBoulder1Pos = new Position(2, 2);
+        Position actualBoulder1Pos = getEntities(res, "boulder").get(0).getPosition();
+        Position expectedBoulder2Pos = new Position(3, 2);
+        Position actualBoulder2Pos = getEntities(res, "boulder").get(1).getPosition();
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulder1Pos, actualBoulder1Pos); 
+        assertEquals(expectedBoulder2Pos, actualBoulder2Pos); 
+
+        res = dmc.tick(Direction.RIGHT);
+        
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulder1Pos, actualBoulder1Pos); 
+        assertEquals(expectedBoulder2Pos, actualBoulder2Pos); 
+    }
+
+    @Test
+    @DisplayName("Check if wall blocks boulder")
+    public void boulderWallBlock() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_boulderTest_wallBlock", "c_playerTest_basicMovement");
+
+        Position expectedPlayerPos = new Position(1, 2);
+        Position actualPlayerPos = getEntities(res, "player").get(0).getPosition();
+        Position expectedBoulderPos = new Position(2, 2);
+        Position actualBoulderPos = getEntities(res, "boulder").get(0).getPosition();
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulderPos, actualBoulderPos); 
+
+        res = dmc.tick(Direction.RIGHT);
+        
+        assertEquals(expectedPlayerPos, actualPlayerPos); 
+        assertEquals(expectedBoulderPos, actualBoulderPos); 
     }
 }
